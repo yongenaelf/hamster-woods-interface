e4299ba4bbedc3a7e7823e4c480449fb659cb465
liujiaqi <zack@beangotown.com>
2023-08-10 16:11:58 +0800

    feat: fix bug

--- src/constants/platform.ts
+++ src/constants/platform.ts
@@ -1,6 +1,6 @@
 import { SupportedELFChainId } from 'types';
 
-export const Network = 'MAIN';
-export const ChainId = SupportedELFChainId.MAIN_NET;
+export const Network = 'TESTNET';
+export const ChainId = SupportedELFChainId.TDVW_NET;
 export const portKeyExtensionUrl =
   'https://chrome.google.com/webstore/detail/portkey-did-crypto-nft/hpjiiechbbhefmpggegmahejiiphbmij';
