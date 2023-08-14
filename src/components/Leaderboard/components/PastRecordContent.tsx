364fb4444180539448402fc79b7363463645887f
Loong Yong En <yeloong@beangotown.com>
2023-08-14 16:56:07 +0800

    feat: mobile past record

--- src/components/Leaderboard/components/PastRecordContent.tsx
+++ src/components/Leaderboard/components/PastRecordContent.tsx
@@ -4,9 +4,14 @@ import { useRankingSeasonHis } from '../data/useRankingSeasonHis';
 import { useIsMobile } from 'redux/selector/mobile';
 
 const DiagonalContainer = ({ icon, leftText, value }: { icon: React.ReactNode; leftText: string; value?: number }) => {
+  const isMobile = useIsMobile();
+
   return (
     <div className="mb-4 flex justify-between rounded-[7px] border-[#00335C] bg-[#0538C9] shadow-[0px_0px_4px_rgba(0,0,0,0.25)_inset]">
-      <div className="flex w-1/2 items-center rounded-bl-[7px] rounded-tl-[7px] bg-white bg-opacity-10">
+      <div
+        className={`flex items-center rounded-bl-[7px] rounded-tl-[7px] bg-white bg-opacity-10 ${
+          isMobile ? 'w-3/5' : 'w-1/2'
+        }`}>
         {icon}
         <span className="mr-4 font-roboto font-bold text-white">{leftText}</span>
         <div className="ml-auto h-full w-8 diagonal-bg-[#0538C9]"></div>
@@ -52,31 +57,28 @@ export const PastRecordContent = () => {
           {dateString}
         </div>
       </div>
-      <div
-        className={`h-1 w-full flex-grow overflow-auto rounded-bl-2xl rounded-br-2xl bg-[#144CEA] p-4 shadow-inner ${
-          isMobile ? 'text-md' : 'text-3xl'
-        }`}>
-        <div className="flex gap-32 p-8">
-          <div className="flex-1">
-            <img
-              src={require('assets/images/past-record-icon.png').default.src}
-              className="mx-auto w-[22rem] px-4 pb-8"
-              alt="Past Record"
-            />
-            <div>
-              <DiagonalContainer
-                icon={<img src={require('assets/images/crown.png').default.src} alt="crown" className="m-4 w-8" />}
-                leftText="Ranking"
-                value={his?.season.rank}
+      {isMobile ? (
+        <>
+          <div className="text-md h-1 w-full flex-grow overflow-auto rounded-bl-2xl rounded-br-2xl bg-[#144CEA] p-4 shadow-inner">
+            <div className="mb-8 flex">
+              <img
+                src={require('assets/images/past-record-icon.png').default.src}
+                className="mr-2 h-auto w-1/3"
+                alt="Past Record"
               />
-              <DiagonalContainer
-                icon={<img src={require('assets/images/bean.png').default.src} alt="crown" className="m-4 w-8" />}
-                leftText="Points"
-                value={his?.season.score}
-              />
+              <div className="flex-1">
+                <DiagonalContainer
+                  icon={<img src={require('assets/images/crown.png').default.src} alt="crown" className="m-4 w-8" />}
+                  leftText="Ranking"
+                  value={his?.season.rank}
+                />
+                <DiagonalContainer
+                  icon={<img src={require('assets/images/bean.png').default.src} alt="crown" className="m-4 w-8" />}
+                  leftText="Points"
+                  value={his?.season.score}
+                />
+              </div>
             </div>
-          </div>
-          <div className="flex-1">
             <table className="w-full text-white">
               <thead className="bg-white bg-opacity-20 text-white text-opacity-70">
                 <tr>
@@ -96,8 +98,54 @@ export const PastRecordContent = () => {
               </tbody>
             </table>
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <>
+          <div className="h-1 w-full flex-grow overflow-auto rounded-bl-2xl rounded-br-2xl bg-[#144CEA] p-4 text-3xl shadow-inner">
+            <div className="flex gap-32 p-8">
+              <div className="flex-1">
+                <img
+                  src={require('assets/images/past-record-icon.png').default.src}
+                  className="mx-auto w-[22rem] px-4 pb-8"
+                  alt="Past Record"
+                />
+                <div>
+                  <DiagonalContainer
+                    icon={<img src={require('assets/images/crown.png').default.src} alt="crown" className="m-4 w-8" />}
+                    leftText="Ranking"
+                    value={his?.season.rank}
+                  />
+                  <DiagonalContainer
+                    icon={<img src={require('assets/images/bean.png').default.src} alt="crown" className="m-4 w-8" />}
+                    leftText="Points"
+                    value={his?.season.score}
+                  />
+                </div>
+              </div>
+              <div className="flex-1">
+                <table className="w-full text-white">
+                  <thead className="bg-white bg-opacity-20 text-white text-opacity-70">
+                    <tr>
+                      <th className="w-1/2 p-4 text-left">Time</th>
+                      <th className="py-4 text-left">Points</th>
+                      <th className="p-4 text-right">Ranking</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {his?.weeks.map((i, idx) => (
+                      <tr key={idx} className="border-b border-white border-opacity-40">
+                        <td className="p-4">Week {idx + 1}</td>
+                        <td className="py-4">{i.score || <div className="text-white text-opacity-60">Not in</div>}</td>
+                        <td className="p-4 text-right">{i.rank}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
