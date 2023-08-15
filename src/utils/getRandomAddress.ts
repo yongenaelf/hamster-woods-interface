2198e6440463562236063c6dd26211a0f5a4b70a
Loong Yong En <yeloong@beangotown.com>
2023-08-15 17:41:33 +0800

    chore: remove unused

--- src/utils/getRandomAddress.ts
+++
@@ -1,9 +0,0 @@
-import { randomBytes } from 'crypto';
-
-/**
- * Generate a random address string for testing purposes.
- * @returns random address string
- */
-export function getRandomAddress() {
-  return `ELF_${randomBytes(20).toString('hex')}_AELF`;
-}
