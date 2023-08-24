8e7d59946534e31e03f4ae651403b709b934766f
Loong Yong En <yeloong@beangotown.com>
2023-08-25 02:44:32 +0800

    revert: changes to commonmodal

--- src/components/CommonModal/index.tsx
+++ src/components/CommonModal/index.tsx
@@ -3,22 +3,14 @@ import { CloseIcon } from 'assets/images/index';
 
 import styles from './style.module.css';
 
-export default function CommonModal({
-  children,
-  title,
-  onCancel,
-  closable = true,
-  className,
-  childWrapperClassName = 'px-4',
-  ...params
-}: ModalProps & { childWrapperClassName?: string }) {
+export default function CommonModal({ children, title, onCancel, closable = true, className, ...params }: ModalProps) {
   return (
     <Modal className={`${styles.commonModal} ${className}`} {...params} closable={false}>
       <Row className={`${styles.modal__title} modal__title`} justify="center" align="middle">
         <span>{title}</span>
         {closable && <CloseIcon className={styles.modal__closeIcon} onClick={onCancel} />}
       </Row>
-      <div className={childWrapperClassName}>{children}</div>
+      <div className="px-4">{children}</div>
     </Modal>
   );
 }
