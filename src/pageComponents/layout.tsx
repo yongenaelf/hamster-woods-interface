f855aaa46e328c57c136fde6693452731d1e189c
zack <zack@beangotown.com>
2023-08-31 19:46:12 +0800

    feat: fix loading page fontSize

--- src/pageComponents/layout.tsx
+++ src/pageComponents/layout.tsx
@@ -34,164 +34,167 @@ ConfigProvider.setGlobalConfig({
   graphQLUrl: configInfo.configInfo?.graphqlServer,
 });
 
-const Layout = dynamic(async () => {
-  return (props: React.PropsWithChildren<{}>) => {
-    const { children } = props;
+const Layout = dynamic(
+  async () => {
+    return (props: React.PropsWithChildren<{}>) => {
+      const { children } = props;
 
-    const unLoadSourceRef = useRef(0);
+      const unLoadSourceRef = useRef(0);
 
-    const [hasLoadedSource, setHasLoadedSource] = useState(false);
+      const [hasLoadedSource, setHasLoadedSource] = useState(false);
 
-    const pathname = usePathname();
+      const pathname = usePathname();
 
-    const { isMobile: isMobileStore, chessBoardInfo, imageResources, isLogin } = useGetState();
+      const { isMobile: isMobileStore, chessBoardInfo, imageResources, isLogin } = useGetState();
 
-    const router = useRouter();
+      const router = useRouter();
 
-    const [resurceList, setResurceList] = useState<Array<string>>([]);
+      const [resurceList, setResurceList] = useState<Array<string>>([]);
 
-    const [chessMap, setChessMap] = useState<Array<string>>([]);
+      const [chessMap, setChessMap] = useState<Array<string>>([]);
 
-    const [sourceLoded, setSourceLoded] = useState(false);
+      const [sourceLoded, setSourceLoded] = useState(false);
 
-    const [bgImage, setBgImage] = useState('');
+      const [bgImage, setBgImage] = useState('');
 
-    const loadResourceList = () => {
-      if (!chessMap.length && !resurceList.length) {
-        setTimeout(() => {
-          setHasLoadedSource(true);
-        }, 1000);
-      }
-      const sourceMap = [...resurceList, ...chessMap];
+      const loadResourceList = () => {
+        if (!chessMap.length && !resurceList.length) {
+          setTimeout(() => {
+            setHasLoadedSource(true);
+          }, 1000);
+        }
+        const sourceMap = [...resurceList, ...chessMap];
 
-      const timeTask = new Promise(function (resolve) {
-        setTimeout(resolve, 30000, false);
-      });
-
-      const scheduleTask = new Promise(function (resolve) {
-        const start = Date.now();
-        const schedule = (src: string) => {
-          const img = document.createElement('img');
-          img.src = src;
-          const len = sourceMap.filter((i) => i).length;
-          img.onload = img.onerror = () => {
-            unLoadSourceRef.current = unLoadSourceRef.current + 1;
-            if (unLoadSourceRef.current >= len) {
-              if (Date.now() - start > 3000) {
-                resolve(true);
-              } else {
-                setTimeout(() => {
-                  resolve(true);
-                }, 3000 - (Date.now() - start));
-              }
-            }
-          };
-        };
-        sourceMap.forEach((src) => {
-          schedule(src);
+        const timeTask = new Promise(function (resolve) {
+          setTimeout(resolve, 30000, false);
         });
-      });
 
-      Promise.race([timeTask, scheduleTask]).then(() => {
-        console.log('over');
-        setHasLoadedSource(true);
-      });
-    };
-
-    const showHeaderAndFooter = useMemo(() => {
-      return pathname === '/';
-    }, [pathname]);
-
-    useEffect(() => {
-      if (!isLogin) {
-        router.push('/login');
-      }
-      if (typeof window !== undefined) {
-        if (window.localStorage.getItem(KEY_NAME)) {
-          did.reset();
-          store.dispatch(setLoginStatus(LoginStatus.LOCK));
-        }
-      }
-    }, []);
-
-    const initConfigAndResurce = async () => {
-      const [chessBoardRes, configRes] = await Promise.all([fetchChessboardData(), fetchConfigItems()]);
-      configRes && store.dispatch(setConfigInfo(configRes.data));
-      chessBoardRes && store.dispatch(setChessboardData(chessBoardRes.data));
-    };
-
-    useEffect(() => {
-      sourceLoded && loadResourceList();
-    }, [sourceLoded]);
-
-    useEffect(() => {
-      if (!chessBoardInfo || !imageResources) return;
-
-      const bgPc = imageResources['aloginBgPc'];
-      const bgM = imageResources['aloginBgMobile'];
-      setBgImage(isMobileStore ? bgM : bgPc);
-
-      let imageResourcesArray: Array<string> = [];
-      for (const picName in imageResources) {
-        if (picName !== 'aloginBgPc' && picName !== 'aloginBgMobile') {
-          imageResourcesArray.push(imageResources[picName]);
-        }
-      }
-      imageResourcesArray = [isMobileStore ? bgM : bgPc, ...imageResourcesArray];
-
-      setResurceList(imageResourcesArray);
-      const chessMap: string[] = [];
-      if (chessBoardInfo.length) {
-        chessBoardInfo.forEach((item, index) => {
-          item.forEach((_, index1) => {
-            chessMap.push(chessBoardInfo[index][index1].image!);
+        const scheduleTask = new Promise(function (resolve) {
+          const start = Date.now();
+          const schedule = (src: string) => {
+            const img = document.createElement('img');
+            img.src = src;
+            const len = sourceMap.filter((i) => i).length;
+            img.onload = img.onerror = () => {
+              unLoadSourceRef.current = unLoadSourceRef.current + 1;
+              if (unLoadSourceRef.current >= len) {
+                if (Date.now() - start > 3000) {
+                  resolve(true);
+                } else {
+                  setTimeout(() => {
+                    resolve(true);
+                  }, 3000 - (Date.now() - start));
+                }
+              }
+            };
+          };
+          sourceMap.forEach((src) => {
+            schedule(src);
           });
         });
-      }
-      setChessMap(chessMap);
-      setSourceLoded(true);
-    }, [chessBoardInfo]);
 
-    useEffect(() => {
-      initConfigAndResurce();
-
-      const resize = () => {
-        const ua = navigator.userAgent;
-        const mobileType = isMobile(ua);
-        const isMobileDevice =
-          mobileType.apple.phone || mobileType.android.phone || mobileType.apple.tablet || mobileType.android.tablet;
-        store.dispatch(setIsMobile(isMobileDevice));
+        Promise.race([timeTask, scheduleTask]).then(() => {
+          console.log('over');
+          setHasLoadedSource(true);
+        });
       };
-      resize();
-      window.addEventListener('resize', resize);
-      return () => {
-        window.removeEventListener('resize', resize);
-      };
-    }, []);
 
-    return hasLoadedSource ? (
-      <>
-        <AntdLayout className="xx-wrapper flex h-full w-[100vw] flex-col overflow-hidden">
-          {showHeaderAndFooter && isMobileStore && <Header />}
-          <AntdLayout.Content className="marketplace-content flex-1 overflow-hidden" id="marketplace-content">
-            {children}
-            <Leaderboard />
-            <GameRecord />
-          </AntdLayout.Content>
-          <PageLoading />
-        </AntdLayout>
-      </>
-    ) : (
-      <>
-        <LoadingAnimation />
-        <img
-          src={bgImage}
-          alt=""
-          className="w-[100vw] h-[100vh] absolute top-0 left-0 !bg-cover bg-center bg-no-repeat z-[-1000]"
-        />
-      </>
-    );
-  };
-});
+      const showHeaderAndFooter = useMemo(() => {
+        return pathname === '/';
+      }, [pathname]);
+
+      useEffect(() => {
+        if (!isLogin) {
+          router.push('/login');
+        }
+        if (typeof window !== undefined) {
+          if (window.localStorage.getItem(KEY_NAME)) {
+            did.reset();
+            store.dispatch(setLoginStatus(LoginStatus.LOCK));
+          }
+        }
+      }, []);
+
+      const initConfigAndResurce = async () => {
+        const [chessBoardRes, configRes] = await Promise.all([fetchChessboardData(), fetchConfigItems()]);
+        configRes && store.dispatch(setConfigInfo(configRes.data));
+        chessBoardRes && store.dispatch(setChessboardData(chessBoardRes.data));
+      };
+
+      useEffect(() => {
+        sourceLoded && loadResourceList();
+      }, [sourceLoded]);
+
+      useEffect(() => {
+        if (!chessBoardInfo || !imageResources) return;
+
+        const bgPc = imageResources['aloginBgPc'];
+        const bgM = imageResources['aloginBgMobile'];
+        setBgImage(isMobileStore ? bgM : bgPc);
+
+        let imageResourcesArray: Array<string> = [];
+        for (const picName in imageResources) {
+          if (picName !== 'aloginBgPc' && picName !== 'aloginBgMobile') {
+            imageResourcesArray.push(imageResources[picName]);
+          }
+        }
+        imageResourcesArray = [isMobileStore ? bgM : bgPc, ...imageResourcesArray];
+
+        setResurceList(imageResourcesArray);
+        const chessMap: string[] = [];
+        if (chessBoardInfo.length) {
+          chessBoardInfo.forEach((item, index) => {
+            item.forEach((_, index1) => {
+              chessMap.push(chessBoardInfo[index][index1].image!);
+            });
+          });
+        }
+        setChessMap(chessMap);
+        setSourceLoded(true);
+      }, [chessBoardInfo]);
+
+      useEffect(() => {
+        initConfigAndResurce();
+
+        const resize = () => {
+          const ua = navigator.userAgent;
+          const mobileType = isMobile(ua);
+          const isMobileDevice =
+            mobileType.apple.phone || mobileType.android.phone || mobileType.apple.tablet || mobileType.android.tablet;
+          store.dispatch(setIsMobile(isMobileDevice));
+        };
+        resize();
+        window.addEventListener('resize', resize);
+        return () => {
+          window.removeEventListener('resize', resize);
+        };
+      }, []);
+
+      return hasLoadedSource ? (
+        <>
+          <AntdLayout className="xx-wrapper flex h-full w-[100vw] flex-col overflow-hidden">
+            {showHeaderAndFooter && isMobileStore && <Header />}
+            <AntdLayout.Content className="marketplace-content flex-1 overflow-hidden" id="marketplace-content">
+              {children}
+              <Leaderboard />
+              <GameRecord />
+            </AntdLayout.Content>
+            <PageLoading />
+          </AntdLayout>
+        </>
+      ) : (
+        <>
+          <LoadingAnimation />
+          <img
+            src={bgImage}
+            alt=""
+            className="w-[100vw] h-[100vh] absolute top-0 left-0 !bg-cover bg-center bg-no-repeat z-[-1000]"
+          />
+        </>
+      );
+    };
+  },
+  { ssr: false },
+);
 
 export default Layout;
