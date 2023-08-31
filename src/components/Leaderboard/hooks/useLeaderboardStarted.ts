9846e2af5b0357bb747e1e9c61beee4496dde7b4
Loong Yong En <yeloong@beangotown.com>
2023-09-01 01:03:38 +0800

    fix: modal does not show if data is undefined

--- src/components/Leaderboard/hooks/useLeaderboardStarted.ts
+++ src/components/Leaderboard/hooks/useLeaderboardStarted.ts
@@ -3,5 +3,7 @@ import { useWeeklyRank } from '../data/useWeeklyRank';
 export const useLeaderboardStarted = () => {
   const { data } = useWeeklyRank();
 
-  return !(data?.status === 0 && data.refreshTime === null);
+  if (!data) return false;
+
+  return !(data.status === 0 && data.refreshTime === null);
 };
