8099860677d7c35c65fa97e620908971390fd539
zack <zack@beangotown.com>
2023-09-06 14:17:15 +0800

    feat: fix jump asset page

--- src/components/Header/components/Wallet/index.tsx
+++ src/components/Header/components/Wallet/index.tsx
@@ -1,13 +1,39 @@
+import { getBeanPassClaimClaimable } from 'api/request';
+import { useAddress } from 'hooks/useAddress';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { useCallback } from 'react';
 import useGetState from 'redux/state/useGetState';
+import showMessage from 'utils/setGlobalComponentsInfo';
 
 export default function Wallet() {
   const { isMobile } = useGetState();
 
   const router = useRouter();
-  const handleAsset = () => {
-    router.push('/asset');
+
+  const address = useAddress();
+
+  const checkAccountInitStatus = useCallback(async () => {
+    showMessage.loading();
+    let checkAccountInitStatusRes;
+    try {
+      checkAccountInitStatusRes = await getBeanPassClaimClaimable({
+        caAddress: address,
+      });
+      console.log('checkAccountInitStatus', checkAccountInitStatusRes);
+      showMessage.hideLoading();
+    } catch (err) {
+      showMessage.hideLoading();
+      console.log('checkBeanPassStatusError:', err);
+      return true;
+    }
+    if (!checkAccountInitStatusRes) return false;
+    return true;
+  }, [address]);
+
+  const handleAsset = async () => {
+    const isAbleInit = await checkAccountInitStatus();
+    isAbleInit && router.push('/asset');
   };
 
   return (
