d00a30ac55817a404ebccc83b370c2bc0b5f70fc
amber-bean <amber@beangotown.com>
2023-09-13 19:08:35 +0800

    feat: add production.js

--- build.config/rewrites/production.js
+++ build.config/rewrites/production.js
@@ -1,6 +1,22 @@
 module.exports = [
   {
+    source: '/connect/:path*',
+    destination: 'https://auth-portkey.portkey.finance/connect/:path*',
+  },
+  {
+    source: '/portkey/:path*',
+    destination: 'https://did-portkey.portkey.finance/:path*',
+  },
+  {
     source: '/api/:path*',
-    destination: 'http://192.168.67.187:8068/api/:path*',
+    destination: 'https://www.beangotown.com/api/:path*',
+  },
+  {
+    source: '/cms/:path*',
+    destination: 'https://cms.beangotown.com/:path*',
+  },
+  {
+    source: '/AElfIndexer_BeangoTown/BeangoTownIndexerPluginSchema/:path*',
+    destination: 'https://www.beangotown.com/AElfIndexer_BeangoTown/BeangoTownIndexerPluginSchema/:path*',
   },
 ];
