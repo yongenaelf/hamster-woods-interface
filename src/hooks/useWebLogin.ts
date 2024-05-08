a88ffd288ad3c7e158b1a334ea08c5923f183d50
skywavejourney <skywavejourney@beangotown.com>
2024-05-08 17:17:33 +0800

    fix: auto unlock infinite loop in telegram

--- src/hooks/useWebLogin.ts
+++ src/hooks/useWebLogin.ts
@@ -322,56 +322,59 @@ export default function useWebLogin({ signHandle }: { signHandle?: any }) {
     handleFinish(WalletType.discover, discoverInfo);
   }, []);
 
-  const handleFinish = async (type: WalletType, walletInfo: PortkeyInfoType | IDiscoverInfo) => {
-    console.log('wallet', type, walletInfo);
+  const handleFinish = useCallback(
+    async (type: WalletType, walletInfo: PortkeyInfoType | IDiscoverInfo) => {
+      console.log('wallet', type, walletInfo);
 
-    if (type === WalletType.discover) {
-      store.dispatch(setWalletType(type));
-      localStorage.setItem(LOGIN_EARGLY_KEY, 'true');
-      setDiscoverInfo(walletInfo);
-      setWallet({ discoverInfo: walletInfo as IDiscoverInfo });
-      setCurWalletType(type);
-      store.dispatch(
-        setWalletInfo({
-          discoverInfo: walletInfo,
-        }),
-      );
-      InstanceProvider.setWalletInfoInstance({
-        discoverInfo: walletInfo,
-      });
-      trackLoginInfo({
-        caAddress: (walletInfo as IDiscoverInfo).address!,
-        caHash: '',
-      });
-      store.dispatch(setLoginStatus(LoginStatus.LOGGED));
-    } else if (type === WalletType.portkey) {
-      did.save((walletInfo as PortkeyInfoType)?.pin || '', KEY_NAME);
-      setDidWalletInfo(walletInfo as PortkeyInfoType);
-      setWallet({
-        portkeyInfo: walletInfo as PortkeyInfoType,
-      });
-      setCurWalletType(type);
-      store.dispatch(setWalletType(WalletType.portkey));
-      if ((walletInfo as PortkeyInfoType).chainId !== curChain) {
-        InstanceProvider.setWalletInfoInstance({
-          portkeyInfo: walletInfo as PortkeyInfoType,
-        });
-        const holder = await getSyncHolder(curChain, walletInfo as DIDWalletInfo);
-        trackLoginInfo({ caAddress: holder.caAddress, caHash: (walletInfo as PortkeyInfoType)!.caInfo!.caHash });
-      } else {
+      if (type === WalletType.discover) {
+        store.dispatch(setWalletType(type));
+        localStorage.setItem(LOGIN_EARGLY_KEY, 'true');
+        setDiscoverInfo(walletInfo);
+        setWallet({ discoverInfo: walletInfo as IDiscoverInfo });
+        setCurWalletType(type);
         store.dispatch(
           setWalletInfo({
-            portkeyInfo: walletInfo,
+            discoverInfo: walletInfo,
           }),
         );
-        trackLoginInfo({
-          caAddress: (walletInfo as PortkeyInfoType)!.caInfo!.caAddress,
-          caHash: (walletInfo as PortkeyInfoType)!.caInfo!.caHash,
+        InstanceProvider.setWalletInfoInstance({
+          discoverInfo: walletInfo,
         });
+        trackLoginInfo({
+          caAddress: (walletInfo as IDiscoverInfo).address!,
+          caHash: '',
+        });
+        store.dispatch(setLoginStatus(LoginStatus.LOGGED));
+      } else if (type === WalletType.portkey) {
+        did.save((walletInfo as PortkeyInfoType)?.pin || '', KEY_NAME);
+        setDidWalletInfo(walletInfo as PortkeyInfoType);
+        setWallet({
+          portkeyInfo: walletInfo as PortkeyInfoType,
+        });
+        setCurWalletType(type);
+        store.dispatch(setWalletType(WalletType.portkey));
+        if ((walletInfo as PortkeyInfoType).chainId !== curChain) {
+          InstanceProvider.setWalletInfoInstance({
+            portkeyInfo: walletInfo as PortkeyInfoType,
+          });
+          const holder = await getSyncHolder(curChain, walletInfo as DIDWalletInfo);
+          trackLoginInfo({ caAddress: holder.caAddress, caHash: (walletInfo as PortkeyInfoType)!.caInfo!.caHash });
+        } else {
+          store.dispatch(
+            setWalletInfo({
+              portkeyInfo: walletInfo,
+            }),
+          );
+          trackLoginInfo({
+            caAddress: (walletInfo as PortkeyInfoType)!.caInfo!.caAddress,
+            caHash: (walletInfo as PortkeyInfoType)!.caInfo!.caHash,
+          });
+        }
+        store.dispatch(setLoginStatus(LoginStatus.LOGGED));
       }
-      store.dispatch(setLoginStatus(LoginStatus.LOGGED));
-    }
-  };
+    },
+    [curChain],
+  );
 
   const loginEagerly = useCallback(async () => {
     const provider = await detect();
