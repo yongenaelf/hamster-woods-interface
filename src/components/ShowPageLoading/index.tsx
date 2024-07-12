7bf28993f131ff21384c60c660a329437b417fc3
sarah-bgt <sunsetharbor@beangotown.com>
2024-07-12 16:16:15 +0800

    fix: 🐛 login loading

---
+++ src/components/ShowPageLoading/index.tsx
@@ -0,0 +1,49 @@
+import { Modal } from 'antd';
+import React from 'react';
+
+import styles from './index.module.css';
+import Lottie from 'lottie-react';
+
+import loadingAnimation from 'assets/images/loading-animation.json';
+import useGetState from 'redux/state/useGetState';
+
+export interface IShowPageLoadingProps {
+  open: boolean;
+  content?: string;
+}
+
+function ShowPageLoading({ open, content }: IShowPageLoadingProps) {
+  const { isMobile } = useGetState();
+
+  const getContent = () => {
+    if (typeof content === 'string') {
+      return (
+        <span className={`text-[#fff] text-center font-paytone ${isMobile ? 'text-[16px]' : 'text-[24px]'}`}>
+          {content}
+        </span>
+      );
+    } else {
+      return content;
+    }
+  };
+
+  return (
+    <Modal
+      open={open}
+      footer={null}
+      keyboard={false}
+      maskClosable={false}
+      closable={false}
+      destroyOnClose={true}
+      className={styles['page-modal']}>
+      <div className="relative flex flex-col justify-center">
+        <div className={`${isMobile ? 'w-[110px]' : 'w-[147px]'} m-auto`}>
+          <Lottie animationData={loadingAnimation} />
+        </div>
+        {content && getContent()}
+      </div>
+    </Modal>
+  );
+}
+
+export default React.memo(ShowPageLoading);
