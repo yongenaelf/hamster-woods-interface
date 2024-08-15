ac9105dce3b1094781d4db8828711149dea1d212
ericana-beangotown <ericana@beangotown.com>
2024-08-15 18:26:40 +0800

    fix: chain is not running error

--- src/contract/contractRequest.ts
+++ src/contract/contractRequest.ts
@@ -118,7 +118,7 @@ export default class ContractRequest {
       chainsList.forEach((item) => {
         this.chainInfoMap![item.chainId] = item;
       });
-      chainInfo = this.chainInfoMap?.[chainId];
+      chainInfo = this.chainInfoMap?.[this.chainId!];
     }
     if (!chainInfo) {
       throw new Error(`Chain is not running: ${this.chainId}`);
