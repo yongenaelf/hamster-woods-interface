07501614e7042f24417f8e292c3940fbc570befe
kevin flynn <kevin.flynn@portkey.finance>
2024-10-25 16:23:58 +0800

    chore(build): cherry-pick imports for antd and lodash then analyze

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const pluginConfig = require('./build.config/plugin');
 const development = require('./build.config/development');
 const production = require('./build.config/production');
 
+const withBundleAnalyzer =
+  process.env.ANALYZE === 'true'
+    ? require('@next/bundle-analyzer')({
+        enabled: true,
+      })
+    : (x) => x;
 const config = NEXT_PUBLIC_APP_ENV === 'production' ? production : development;
 
-module.exports = withPlugins(pluginConfig, config);
+module.exports = withPlugins(
+  pluginConfig,
+  withBundleAnalyzer({
+    ...config,
+    // output: 'standalone',  // TODO after updating Dockerfile
+  }),
+);
